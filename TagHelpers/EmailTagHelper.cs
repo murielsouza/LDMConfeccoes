63f74ca06c635ccf27abfd5b87235bbda06deb4a
murielsouza <muriel-souza@live.com>
2022-10-16 18:35:31 -0300

    últimas alterações

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LDMConfeccoes.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+       public string Endereco { get; set; }
+       public string Conteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:".Concat(Endereco));
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
